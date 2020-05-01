ca96d7347d4622ee3dfb82b2c10d80d89d88dcb2
[Christopher <wisechristopher87@gmail.com>
2020-04-30 20:08:59 -0500

    Updated Automation Tests

--- Automation/TC_02_ADD_PET_AUTOMATION (ruby) (2.1).rb
+++ Automation/TC_02_ADD_PET_AUTOMATION (ruby) (2.1).rb
@@ -1,16 +1,23 @@
 require 'watir'
 
+host_name = 'http://admin:admin@clinic.doveryai-no-proveryai.com:9966/petclinic/api/pettypes'
+
+T=DateTime.now.strftime("%Q")
+DogT= "Dog#{T}"
+puts "Timestamp is #{T}"
+
+
 #A user must have permissions in order to view/add Pet Types' 
 
 #Intialize the Browser
 
 browser = Watir::Browser.new
-sleep 5
+#sleep 5
 
 #Navigate to the page & Login to the website as an Owner_Admin
 
 browser.goto 'http://admin:admin@clinic.doveryai-no-proveryai.com:9966/petclinic/swagger-ui.html'
-sleep 5
+#sleep 5
 
 #Navigate back to website to view all of the pet types 
 
@@ -22,21 +29,21 @@ browser.button(text: 'Add').click
 sleep 5
 
 #Name pet (Time Stamp) 
-browser.text_field(name: "name").set("Cat#{DateTime.now.strftime("%Q")}")
+browser.text_field(name: "name").set(DogT)
 sleep 5 
 
 #Save Pet to database 
 browser.button(text: 'Save').click
 sleep 5 
+browser.goto 'http://admin:admin@clinic.doveryai-no-proveryai.com:9966/petclinic/api/pettypes'
+sleep 5
 
-#Curl request to verify that pet was created
+#Request to verify that pet was created
 
-Kernel.system "curl -X GET #{admin:admin@http://clinic.doveryai-no-proveryai.com:9966/petclinic/api/pettypes}"
-
-if browser.text.include? Cat#{DateTime.now.strftime("%Q")}
+if browser.text.include? DogT
 puts "Test pass"
 else
 puts "Test Fail"
 end
-
+sleep 5
 browser.close
--- Automation/TC_03_ADD_OWNER_AUTOMATION (ruby) (2.1).rb
+++ Automation/TC_03_ADD_OWNER_AUTOMATION (ruby) (2.1).rb
@@ -1,4 +1,9 @@
 require 'watir'
+host_name = 'http://admin:admin@clinic.doveryai-no-proveryai.com:9966/petclinic/api/pettypes'
+
+T=DateTime.now.strftime("%Q")
+BloomT= "Bloom#{T}"
+puts "Timestamp is #{T}"
 
 #A user must have permissions in order to view owners' 
 
@@ -7,7 +12,7 @@ require 'watir'
 browser = Watir::Browser.new
 #sleep 5
 
-#Navigate to the page & Login to the website as an Owner_Admin
+#Navigate to the page & Login to the website as an Admin
 
 browser.goto 'http://admin:admin@clinic.doveryai-no-proveryai.com:9966/petclinic/swagger-ui.html'
 #sleep 5
@@ -17,27 +22,27 @@ browser.goto 'http://admin:admin@clinic.doveryai-no-proveryai.com:9966/petclinic
 browser.goto 'https://clinic.doveryai-no-proveryai.com/petclinic/owners/add'
 #sleep 5
 
-#Add a an Owner to the system  
+#Add an Owner information to the system  
 browser.text_field(name: "firstName").set("Orlando")
-browser.text_field(name: "lastName").set("Bloom#{DateTime.now.strftime("%Q")}")
+browser.text_field(name: "lastName").set(BloomT)
 browser.text_field(name: "address").set("4242 Wallaby Way")
 browser.text_field(name: "city").set("Sydney")
 browser.text_field(name: "telephone").set("5128675309")
 sleep 10
 
-#Save Owner to database
+#Add owner to database
 browser.button(text: 'Add Owner').click
 sleep 5 
 
+browser.goto 'http://admin:admin@clinic.doveryai-no-proveryai.com:9966/petclinic/api/owners'
+sleep 5
 
-#Curl request to verify that owner was created
+#Request to verify that owner was created
 
-Kernel.system "curl -X GET #{admin:admin@http://clinic.doveryai-no-proveryai.com:9966/petclinic/api/owners"
-
-if browser.text.include? Orlando
+if browser.text.include? BloomT
 puts "Test pass"
 else
 puts "Test Fail"
 end
-
+sleep 5
 browser.close
